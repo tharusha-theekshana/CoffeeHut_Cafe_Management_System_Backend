ef4a94cb8f0b275b9bacf900ac5f8000ecdea815
Theekshana <tharushatheekshana@gmail.com>
2024-11-02 16:01:51 +0530

    Initial commit

---
+++ src/main/java/com/cafe_mn_system/coffeehut_backend/CoffeehutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cafe_mn_system.coffeehut_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeehutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeehutBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe_mn_system/coffeehut_backend/CoffeehutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe_mn_system.coffeehut_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeehutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
